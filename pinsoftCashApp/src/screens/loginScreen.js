d49af87fb06838284c95fa4aac6eed0e414ebc5c
İsmail UZUNDAĞ <77495431+ismailuzundag1@users.noreply.github.com>
2023-09-12 10:19:34 +0300

    TransactionsScreen

--- pinsoftCashApp/src/screens/loginScreen.js
+++ pinsoftCashApp/src/screens/loginScreen.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native"
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+import TransactionsScreen from './TransactionsScreen'; // TransactionsScreen dosyasının yolunu belirtin
 
-const loginScreen = () => {
-  return <Text>loginScreen</Text>;
-};
+class LoginScreen extends Component {
+  // ... Diğer kodlar ...
 
-export default loginScreen;
+  handleLogin = () => {
+    // Kullanıcı giriş yaptığında TransactionsScreen sayfasına yönlendirin
+    this.props.navigation.navigate('TransactionsScreen'); // 'TransactionsScreen' sayfasının adını kullanın
+  };
+
+  render() {
+    return (
+      <View>
+        {/* ... Diğer bileşenler */}
+        <Button title="Giriş Yap" onPress={this.handleLogin} />
+      </View>
+    );
+  }
+}
+
+export default LoginScreen;
